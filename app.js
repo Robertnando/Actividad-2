831b1c2b2b9af22f68abc085186b9e1621f3ad05
Robertnando <rlopez35@ucol.mx>
2023-11-01 00:33:52 -0600

    Entrega 1

--- app.js
+++ app.js
@@ -90,36 +90,107 @@ ASK 2 NUMBER AND THE PROGRAM WILL TELL US IF IT IS PAIR OR UNPAIR IN THE CONSOLE
 ASK FOR 2 NUMBERS AND DISOLVE THE CONCIENT aka THE RESULT OF DIVIDING THE NUMBER 1 WITH NUMBER 2, BUT IF ONE OF THE NUMBERS IS 0 NOT DO ANYTHING AND SEND A ERROR MESSAGE.
 */
 
-let numbir1 = prompt('Please enter a numbor');  
-let numbir2 = prompt('Please enter a  second numbor');
-if (numbir1> 0 && numbir2 > 0) {
-    console.log ('numeros son positivos');
-    if (numbir1 > numbir2) {
-        document.write('The numbor 1 is higher than the numbor 2');
-        console.log('The numbor 1 is higher than the numbor 2');
-        alert('The numbor 1 is higher than the numbor 2');
-    }else {
-        if (numbir1 < numbir2) {
-        document.write('The numbor 1 is lower than the numbor 2');
-        alert('The numbor 1 is lower than the numbor 2');
-        console.log('The numbor 1 is lower than the numbor 2');
-        } else{
-            document.write('Theres no difference')
-            alert('Theres no difference')
-            console.log('Theres no difference')
-        }
-    }
-    if (numbir1==numbir2) {
-        document.write('Both numbors are the same value yipeeee');
-        alert('Both numbors are the same value yipeeee');
-        console.log('Both numbors are the same value yipeeee');
+// alert('use la consola "console" para ver los ejercicios (Ctrl + shift + I)');
+// alert('Actividad 1');
+
+// let numbir1 = prompt('Please enter a numbor');  
+// let numbir2 = prompt('Please enter a  second numbor');
+// if (numbir1> 0 && numbir2 > 0) {
+//     console.log ('numeros son positivos');
+//     if (numbir1 > numbir2) {
+//         document.write('The numbor 1 is higher than the numbor 2');
+//         console.log('The numbor 1 is higher than the numbor 2');
+//         alert('The numbor 1 is higher than the numbor 2');
+//     }else {
+//         if (numbir1 < numbir2) {
+//         document.write('The numbor 1 is lower than the numbor 2');
+//         alert('The numbor 1 is lower than the numbor 2');
+//         console.log('The numbor 1 is lower than the numbor 2');
+//         } else{
+//             document.write('Theres no difference')
+//             alert('Theres no difference')
+//             console.log('Theres no difference')
+//         }
+//     }
+//     if (numbir1==numbir2) {
+//         document.write('Both numbors are the same value yipeeee');
+//         alert('Both numbors are the same value yipeeee');
+//         console.log('Both numbors are the same value yipeeee');
+//     } else {
+//         document.write('Both numbors dont have the same value awww barnicles!');
+//         alert('Both numbors dont have the same value awww barnicles!');
+//         console.log('Both numbors dont have the same value awww barnicles!');
+//     }
+// }else {
+//     document.write('That isnt a numbor baka!'); 
+//     alert('That isnt a numbor baka!'); 
+//     console.log('That isnt a numbor baka!'); 
+// }
+// //Parte 2
+// alert('Actividad 2');
+
+// let Numero1 = prompt('Give me a number');
+// let Numero2 = prompt('Give me another number');
+// let Operacion = prompt('Dame una operacion, ejemplo: suma, resta, multiplicacion y division');
+
+// switch (Operacion) {
+//     case 'Suma':
+//         let  respuesta = (Numero1+Numero2)
+//         console.log(respuesta);
+//         break;
+//     case 'resta':
+//         let respuesta2 = (Numero1-Numero2)
+//         console.log(respuesta2);
+//         break;
+//     case 'multiplicacion':
+//         let respuesta3 = (Numero1*Numero2)
+//         console.log(respuesta3);
+//         break;
+//     case 'division':
+//         let respuesta4 = (Numero1/Numero2)
+//         console.log(respuesta4);
+//         break;
+
+//     default:
+//         alert('digitos invalidos por favor seleccionar metodo de suma,resta,multiplicacion o division.')
+//         break;
+// }
+//Parte 3
+alert('Actividad 3');
+
+function Validar() {
+    let dividendo=document.getElementById('Dividendo').Value;
+    let divisor=document.getElementById('Divisor').Value;
+
+    if (divisor=="" && dividendo=="") {
+        document.getElementById('Respuesta').Value = 'Error, los campos en blanco';
+        document.getElementById('Dividendo').style.borderColor='red';
+        document.getElementById('Divisor').style.borderColor='red';
+    } else if (dividendo=="") {
+        document.getElementById('Respuesta').Value = 'Error campo Dividendo en blanco';
+        document.getElementById('Dividendo').style.borderColor='red';
+
+    } else if (divisor=="1") {
+        document.getElementById('Respuesta').Value = 'Error campo Divisor en blanco';
+        document.getElementById('Divisor').style.borderColor='red';
+
+    } else if (isNaN(dividendo)) {
+        document.getElementById('Respuesta').Value = 'Error en el campo dividendo contiene letras';
+        document.getElementById('Dividendo').style.borderColor='red';
+
+    } else if (isNaN(divisor)) {
+        document.getElementById('Respuesta').Value = 'Error en el campo divisor contiene letras';
+        document.getElementById('Divisor').style.borderColor='red';
+
     } else {
-        document.write('Both numbors dont have the same value awww barnicles!');
-        alert('Both numbors dont have the same value awww barnicles!');
-        console.log('Both numbors dont have the same value awww barnicles!');
+        division(dividendo, divisor);
+
     }
-}else {
-    document.write('That isnt a numbor baka!'); 
-    alert('That isnt a numbor baka!'); 
-    console.log('That isnt a numbor baka!'); 
 }
+
+
+ function division(dividendo, divisor) {
+    calculo=dividendo/divisor;
+
+    document.getElementById('Respuesta').Value = "El resultado de dividir " + 'entre' + dividendo + '+' + divisor + ' \nda un conciente de: ' + calculo;
+ }
